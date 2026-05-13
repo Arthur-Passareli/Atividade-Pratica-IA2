752978aa935e514abc5e90fb1037dfa432a554d9
Arthur Passareli <arthurpamo23@gmail.com>
2026-05-13 16:08:29 -0400

    Implementação da tela home com novo componente "botão".

---
+++ src/styles/constants.tsx
@@ -0,0 +1,19 @@
+const PRIMARY_COLOR = "#6547e8"
+const PRIMARY_FOREGROUND_COLOR = "#f8f8ff"
+const SUBTITLE_COLOR = "#747477"
+const BORDER_RADIUS_MD = 8
+const BORDER_RADIUS_LG = 12
+
+export const THEME = {
+    colors: {
+        primary: PRIMARY_COLOR,
+        primary_foreground: PRIMARY_FOREGROUND_COLOR,
+        subtitle: SUBTITLE_COLOR
+    },
+    border: {
+        radius: {
+            md: BORDER_RADIUS_MD,
+            lg: BORDER_RADIUS_LG,
+        }
+    }
+}
